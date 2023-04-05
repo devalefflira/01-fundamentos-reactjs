a5be811e5a85bbcaee088f18c6fbf85bc2a0e894
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2023-04-05 14:12:18 -0300

    Finalizando a estilização do Sidebar

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
+import perfil from '../assets/perfil.jpeg';
 
 export function Sidebar() {
   return (
@@ -9,12 +11,14 @@ export function Sidebar() {
       />
       
       <div className={styles.profile}>
+        <img className={styles.avatar} src={perfil}/>
         <strong>Áleff Lira</strong>
         <span>Web Developer</span>
       </div>
       
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
