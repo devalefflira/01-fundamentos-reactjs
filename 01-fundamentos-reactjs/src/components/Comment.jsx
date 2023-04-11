962d539199d7bf19644aa0070676e69a77e0d76c
ALEFF JUNIO SILVA LINS LIRA <dev.alefflira@gmail.com>
2023-04-11 10:00:31 -0300

    Criada a função de like

--- 01-fundamentos-reactjs/src/components/Comment.jsx
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,12 +1,20 @@
 import { Trash, ThumbsUp } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({content, onDeleteComment}) {
+
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     console.log('deletar');
 
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -30,9 +38,9 @@ export function Comment({content, onDeleteComment}) {
         </div>
         
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Curtir <span>20</span>
+            Curtidas Infinitas <span>{likeCount}</span>
           </button>
         </footer>
 
